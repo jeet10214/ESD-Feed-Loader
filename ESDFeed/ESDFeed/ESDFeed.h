af5b5b603e45c25135523b9599900d8e07c81672
jeet10214 <jeet10214@gmail.com>
2024-08-17 20:16:15 +0400

    Create ESDFeed Framework

---
+++ ESDFeed/ESDFeed/ESDFeed.h
@@ -0,0 +1,18 @@
+//
+//  ESDFeed.h
+//  ESDFeed
+//
+//  Created by Jeet Kapadia on 17/08/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ESDFeed.
+FOUNDATION_EXPORT double ESDFeedVersionNumber;
+
+//! Project version string for ESDFeed.
+FOUNDATION_EXPORT const unsigned char ESDFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ESDFeed/PublicHeader.h>
+
+
