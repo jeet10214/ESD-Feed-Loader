f80894e050a141c0a54ac2cb7340175bba322b25
jeet10214 <jeet10214@gmail.com>
2024-08-17 20:16:51 +0400

    Create FeedItem model

---
+++ ESDFeed/ESDFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  ESDFeed
+//
+//  Created by Jeet Kapadia on 17/08/24.
+//
+
+import Foundation
+
+struct FeedItem {
+    var id: UUID
+    var description: String?
+    var location: String?
+    var imageURL: URL
+}
