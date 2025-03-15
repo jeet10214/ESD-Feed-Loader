f9b207c6fa4b0bbb782972933a222597e1878557
jeet10214 <jeet10214@gmail.com>
2025-03-15 19:19:40 +0400

    Local command delivers cached images on less than seven days old cache

--- EssentialFeed/EssentialFeed/FeedCache/FeedStore.swift
+++ EssentialFeed/EssentialFeed/FeedCache/FeedStore.swift
@@ -8,10 +8,16 @@
 
 import Foundation
 
+public enum RetrieveCacheFeedResult {
+    case empty
+    case found(feed: [LocalFeedImage], timestamp: Date)
+    case failure(error: Error)
+}
+
 public protocol FeedStore: AnyObject {
     typealias DeletionsCompletion = (Error?) -> (Void)
     typealias InsertionsCompletion = (Error?) -> (Void)
-    typealias RetrievalCompletion = (Error?) -> (Void)
+    typealias RetrievalCompletion = (RetrieveCacheFeedResult) -> (Void)
     
     func deleteCachedFeed(completion: @escaping DeletionsCompletion)
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionsCompletion)
--- EssentialFeed/EssentialFeed/FeedCache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/FeedCache/LocalFeedLoader.swift
@@ -30,11 +30,15 @@ public final class LocalFeedLoader {
     }
     
     public func load(completion: @escaping (LoadResult) -> Void) {
-        store.retrieve { error in
-            if let error {
+        store.retrieve { result in
+            switch result {
+            case let .failure(error):
                 completion(.failure(error))
-            } else {
+            case .empty:
                 completion(.success([]))
+            case let .found(feed, _):
+                completion(.success(feed.toModels()))
+                break
             }
         }
     }
@@ -50,5 +54,11 @@ public final class LocalFeedLoader {
 extension Array where Element == FeedImage {
     func toLocal() -> [LocalFeedImage] {
         self.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
+    }
+}
+
+extension Array where Element == LocalFeedImage {
+    func toModels() -> [FeedImage] {
+        self.map { FeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
     }
 }
